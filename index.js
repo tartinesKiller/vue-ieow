870a5bcaecb60d6f42a6fbd69a19207e007026a3
Damien Pijaczynski <damien.pijaczynski@gmail.com>
2019-02-10 19:23:08 +0100

    Added idow: await for a variable to have different value than the specified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export default {
     install: function (Vue) {
         Vue.prototype.$ieow = ieow;
+        Vue.prototype.$idow = idow;
     },
 };
 
@@ -23,6 +24,24 @@ const ieow = async function (path, awaitedValue) {
         }
     });
 };
+const idow = async function (path, notAwaitedValue) {
+    return new Promise((resolve, reject) => {
+        const curValue = resolvePath(path, this);
+        if (curValue !== notAwaitedValue) {
+            console.log(`${path} have not already the not-awaited value, resolving`);
+            resolve();
+        } else { // if not, wait for the value to change
+            console.log(`${path} doesn't have not-awaited value yet, will wait`);
+            const unwatch = this.$watch(path, newVal => {
+                if (newVal !== notAwaitedValue) {
+                    console.log(`${path} have a different value than the not-awaited, resolving and unwatch`);
+                    unwatch();
+                    resolve();
+                }
+            });
+        }
+    });
+}
 
 function resolvePath (path, obj = self, separator = ".") {
     var properties = Array.isArray(path) ? path : path.split(separator);
